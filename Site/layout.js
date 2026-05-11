ae866cd86054d569e59a2cd76edd08ee09b2c245
Fyve <186026883+Fyve505@users.noreply.github.com>
2026-05-11 11:13:31 +0400

    Update layout.js

--- Site/layout.js
+++ Site/layout.js
@@ -1,60 +1,62 @@
-class SiteLayout extends HTMLElement {
-    connectedCallback() {
-        const main = this.querySelector("main");
-
-        this.innerHTML = `
-<header>
-<h1>
-  <a href="/" style="text-decoration: none; color: inherit;">
-    <img src="/favicon.ico" width="48">
-    BlueCube
-  </a>
-</h1>
-
 <nav>
-    <a href="/">Home</a>
+    <a href="/"><i class="fa-solid fa-home"></i> Home</a>
+
+    <div class="dropdown">
+        <button class="dropbtn">
+            <i class="fa-solid fa-screwdriver-wrench"></i>
+            Tools
+        </button>
+
+        <div class="dropdown-content">
+            <a href="/tools/Invisible-Text">
+                <i class="fa-solid fa-message"></i>
+                Hidden-MSG
+            </a>
+        </div>
+    </div>
+
+    <div class="dropdown">
+        <button class="dropbtn">
+            <i class="fa-solid fa-link"></i>
+            Links
+        </button>
+
+        <div class="dropdown-content">
+            <a href="https://github.com/Fyve505/BlueCube">
+                <i class="fa-brands fa-github"></i>
+                Github
+            </a>
+
+            <a href="#">
+                <i class="fa-brands fa-discord"></i>
+                Discord
+            </a>
+        </div>
+    </div>
+
+    <div class="dropdown">
+        <a href="/user">
+            <button class="dropbtn">
+                <i class="fa-solid fa-users"></i>
+                Users
+            </button>
+        </a>
+
+        <div class="dropdown-content">
+            <a href="/user/Five2357">
+                <i class="fa-solid fa-user"></i>
+                Five2357
+            </a>
+
+            <a href="/user/Glitch.html.html">
+                <i class="fa-solid fa-user-secret"></i>
+                GlitchGuy
+            </a>
+        </div>
+    </div>
 
     <button id="themeToggle">
+        <i class="fa-solid fa-wind theme-toggle"></i>
         Theme
     </button>
 </nav>
-</header>
-
-<main id="page-content"></main>
-
-<footer>
-    <p>© 2025 BlueCube</p>
-</footer>
-`;
-
-        // PUT ORIGINAL MAIN BACK
-        if (main) {
-            this.querySelector("#page-content").replaceWith(main);
-        }
-
-        // THEME
-        const toggleBtn = document.getElementById("themeToggle");
-
-        function applyTheme(theme) {
-            document.body.classList.toggle("dark", theme === "dark");
-            localStorage.setItem("theme", theme);
-        }
-
-        const savedTheme = localStorage.getItem("theme");
-
-        if (savedTheme) {
-            applyTheme(savedTheme);
-        }
-
-        toggleBtn.addEventListener("click", () => {
-            const newTheme =
-                document.body.classList.contains("dark")
-                    ? "light"
-                    : "dark";
-
-            applyTheme(newTheme);
-        });
-    }
-}
-
-customElements.define("site-layout", SiteLayout);
